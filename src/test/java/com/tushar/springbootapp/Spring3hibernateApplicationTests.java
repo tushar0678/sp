46f2be55cf4af6007c7ad51510c42e89fe76fc9e
tushar <tushar@>
2022-04-30 16:53:22 +0530

    finally spring application done

---
+++ src/test/java/com/tushar/springbootapp/Spring3hibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tushar.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3hibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
